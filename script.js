140b19b9d93d68aaf8044a08aa4b4b341c60da20
sujeet-patil <patilsujeet46@gmail.com>
2022-06-20 16:59:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggles = document.querySelectorAll(".toggle");
+
+const good = document.querySelector("#good");
+const cheap = document.querySelector("#cheap");
+const fast = document.querySelector("#fast");
+
+toggles.forEach((toggle)=> toggle.addEventListener("change",(e)=>{
+    transitionFunc(e.target);
+}))
+
+function transitionFunc(theClickedOne){
+    if(good.checked && fast.checked && cheap.checked){
+        if(good === theClickedOne){
+            fast.checked =  false;
+        }
+        if(cheap===theClickedOne){
+            good.checked=false;
+        }
+        if(fast===theClickedOne){
+            cheap.checked=false;
+        }
+    }
+}
